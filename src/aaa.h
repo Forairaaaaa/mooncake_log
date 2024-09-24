18039d9b6805b7995c181274c51cbf95d04338e6
海底撩 <applesyqd@outlook.com>
2024-09-24 21:06:19 +0800

    Initial commit

---
+++ src/aaa.h
@@ -0,0 +1,17 @@
+/**
+ * @file aaa.h
+ * @author Forairaaaaa
+ * @brief
+ * @version 0.1
+ * @date 2024-09-24
+ *
+ * @copyright Copyright (c) 2024
+ *
+ */
+#pragma once
+
+class aaa
+{
+public:
+    static void qiu_ni_le();
+};
